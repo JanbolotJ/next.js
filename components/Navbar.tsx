6fc09a82c8637f1ec7dca3dcc31e4d80d8e8638c
Janbolot <Janbolot>
2024-10-22 22:43:12 +0300

    burger

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React, { useState } from 'react';
 import cls from "@/styles/Navbar.module.scss";
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -11,13 +11,23 @@ const navigation = [
     {id:4, title: 'Admin', path: '/admin'}
 ]
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const {pathname} = useRouter();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <nav className={cls.navbar}>
       <div className={cls.logo}>
         <h1>News</h1>
       </div>
-      <div className={cls.links}>
+      <div className={cls.burger} onClick={toggleMenu}>
+        <div className={isOpen ? cls.lin1 : cls.line}></div>
+        <div className={isOpen ? cls.lin2 : cls.line}></div>
+        <div className={isOpen ? cls.lin3 : cls.line}></div>
+      </div>
+      <div className={isOpen ? cls.navActive : cls.navLinks}>
         {navigation.map(({id, title, path}) => (
             <Link key={id} href={path} className={pathname === path ? cls.active : undefined}>
               {title}
